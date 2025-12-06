a7c9c6ea9a01b8a2a1f20ec5eda6d937534ab4c5
yutong122 <s10274433@connect.np.edu.sg>
2025-12-06 17:14:29 +0800

    update5

--- script.js
+++ script.js
@@ -11,18 +11,18 @@ buttons.forEach(btn => {
 //submit button
 
 function showAlert() {
-// get values from input
-    let name = document.getElementById("name").value.trim();  
-    let email = document.getElementById("email").value.trim();  
+    // get values from input
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
     let mobile = document.getElementById("mobile").value.trim();
     let message = document.getElementById("message").value.trim();
 
-//check if any field is empty
-    if(!name || !email || !mobile || !message){
+    //check if any field is empty
+    if (!name || !email || !mobile || !message) {
         alert("Please type something!")
         return;
     }
 
-//only if all is filled
+    //only if all is filled
     alert("Thank you so much and our team will review and respond accordingly.")
 }
